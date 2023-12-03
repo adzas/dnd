1e43bc5af34928cd42decc3867bbb263fcf903a8
Andrzej Nogala <andrzejojo@gmail.com>
2023-12-03 15:22:50 +0100

    Updates

---
+++ precache-manifest.fc210106f83330ae662268b432bdfa8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643c7435ec419838646f3a808efc0fb6",
+    "url": "/dnd/index.html"
+  },
+  {
+    "revision": "4a0ef1a5351891af88ec",
+    "url": "/dnd/static/css/main.b8b01ab2.chunk.css"
+  },
+  {
+    "revision": "4d24ea480035723c6812",
+    "url": "/dnd/static/js/2.ed9d115e.chunk.js"
+  },
+  {
+    "revision": "279e4fca54e5d8e57f31",
+    "url": "/dnd/static/js/3.33a557c1.chunk.js"
+  },
+  {
+    "revision": "4a0ef1a5351891af88ec",
+    "url": "/dnd/static/js/main.b5b3ad29.chunk.js"
+  },
+  {
+    "revision": "c87d7c94e8a3a56a8f5b",
+    "url": "/dnd/static/js/runtime~main.1b2199b3.js"
+  }
+]);
--- src/enemy-type/ruster.js
+++
@@ -1,7 +0,0 @@
-import Enemy from "../basic/enemy";
-
-export default class Ruster extends Enemy {
-    shortInfo() {
-        return 'Rdzewnik gryzie ><';
-    };
-}
